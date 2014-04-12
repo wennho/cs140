733497187acb56f768e413a7a943bd889d385e7a
Wen Hao Lui <whlui@stanford.edu>
2014-04-12 16:54:44 -0700

    reverted donate-one test. accidentally saved changes to it earlier

--- src/tests/threads/priority-donate-one.c
+++ src/tests/threads/priority-donate-one.c
@@ -38,8 +38,6 @@ test_priority_donate_one (void)
   msg ("This thread should have priority %d.  Actual priority: %d.",
        PRI_DEFAULT + 2, thread_get_priority ());
   lock_release (&lock);
-  msg ("This thread should have priority %d.  Actual priority: %d.",
-       PRI_DEFAULT, thread_get_priority ());
   msg ("acquire2, acquire1 must already have finished, in that order.");
   msg ("This should be the last line before finishing this test.");
 }
