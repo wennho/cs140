63a9b917c89a2455a1bd822969439257df76a753
Gavin <gavin@birdfamily.us>
2014-04-16 14:10:40 -0700

    Switched to static c file

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -18,3 +18,81 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
   thread_exit ();
 }
+
+static
+void halt (void)
+{
+
+}
+
+static void
+exit (int status)
+{
+
+}
+
+static pid_t
+exec (const char *cmd_line)
+{
+
+}
+
+static int
+wait (pit_t pid)
+{
+
+}
+
+static bool
+create (const char *file, unsigned initial_size)
+{
+
+}
+
+static bool
+remove (const char *file)
+{
+
+}
+
+static int
+open (const char *file)
+{
+
+}
+
+static int 
+filesize (int fd)
+{
+
+}
+
+static int
+read (int fd, void *buffer, unsigned size)
+{
+
+}
+
+static int
+write (int fd, const void *buffer, unsigned size)
+{
+
+}
+
+static void
+seek (int fd, unsigned position)
+{
+
+}
+
+static unsigned 
+tell (int fd)
+{
+
+}
+
+static
+void close (int fd)
+{
+	
+}
