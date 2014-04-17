bb0b2b92c807882e39ec4204ca647ffb80fbfae4
Gavin <gavin@birdfamily.us>
2014-04-16 22:34:15 -0700

    small commit

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,6 +7,24 @@
 
 static void syscall_handler (struct intr_frame *);
 
+static void halt (void);
+static void exit (int status);
+
+static pid_t exec (const char *cmd_line);
+
+static int wait (pid_t pid);
+
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned size);
+static int write (int fd, const void *buffer, unsigned size);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
+
 void
 syscall_init (void) 
 {
