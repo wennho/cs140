bebc661d5ef89157f6838474627c493109b513a1
Wen Hao Lui <whlui@stanford.edu>
2014-04-23 15:14:16 -0700

    fix wait issues

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -7,7 +7,7 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-struct list_elem* child_elem_of_current_thread (tid_t child_tid);
+struct list_elem* child_elem_of_current_thread (tid_t child_tid, struct list *list);
 
 #define PROCESS_MAGIC 0xdeadbeef
 #define MAX_CMD_LINE_LENGTH 200
