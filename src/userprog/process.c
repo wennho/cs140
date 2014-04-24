ebba2583a329f35ec1cac97fa56959956b3c63be
Solomon Sia <solomon5@corn04.stanford.edu>
2014-04-24 09:48:34 -0700

    allTestsPassed

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -386,7 +386,6 @@ load (process_info *pinfo, void (**eip) (void), void **esp)
 
   /* Open executable file. */
   file = filesys_open (file_name);
-  file_deny_write(file);
   if (file == NULL) 
     {
       printf ("load: %s: open failed\n", file_name);
