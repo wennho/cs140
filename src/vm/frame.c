2570684118db8f6b1c227c3576b899bb68cc0937
Solomon Sia <solomon5@corn09.stanford.edu>
2014-04-30 15:25:24 -0700

    page_hash undeclared

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,3 +1,35 @@
+#include "vm/frame.h"
 
 
+/* inits the single frame table that the kernel and all programs use */
+struct frame_table * frame_table_init(){
+	return NULL;
+};
+
+/* adds a page to the frame table. 
+returns the page's physical address for use*/
+uint32_t new_frame(struct frame_table *ft UNUSED)
+{
+	uint32_t a = palloc_get_page(PAL_USER);
+
+	//if palloc_get_page fails,
+	//frame must be made free by evicting some page
+	//from its frame.
+
+	
+	return NULL;
+}
+
+
+struct frame * frameToEvict(struct frame_table * ft UNUSED){
+	return NULL;
+}
+
+void removeReferences(strucdt frame * f){
+	f = f;
+}
+
+void writePage(struct frame * f){
+	f=f;
+}
 
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -2,5 +2,24 @@
 #define FRAME_H_
 
 #include <stdbool.h>
+#include <filesys/file.h>
+#include <list.h>
+
+struct frame_table
+{
+   struct list frame_list;
+};
+
+struct frame
+{
+   uint32_t faddr;
+   struct list_elem elem;
+};
+
+struct frame * frameToEvict(struct frame_table * ft);
+void removeReferences(struct frame * f);
+void writePage(struct frame * f);
+
+struct frame_table * frame_table_init();
 
 #endif /* FRAME_H_ */
