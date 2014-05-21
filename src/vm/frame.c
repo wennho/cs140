3c621fd68dfcd5d34f61fc64882d532a81beb33b
Wen Hao Lui <whlui@stanford.edu>
2014-05-21 15:22:03 -0700

    added data lock for eviction

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -185,6 +185,7 @@ static struct frame * frame_get_new(void *vaddr, bool user, struct page_data* da
 
           struct page_data *data = evict->data;
           ASSERT(is_page_data (data));
+          lock_acquire(&data->lock);
 
           /* Check to make sure that this is an actual mapped file. */
           if (data->is_mapped && data->backing_file->mapping != -1)
@@ -196,6 +197,7 @@ static struct frame * frame_get_new(void *vaddr, bool user, struct page_data* da
             {
               swap_write_page (evict);
             }
+          lock_release(&data->lock);
           /* Relock after IO finished. */
           lock_acquire (&frame_table->lock);
         }
