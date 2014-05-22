e27c04bb9a043627a5b496593ef8c7a11e827560
Gavin <gavin@birdfamily.us>
2014-05-21 21:16:24 -0700

    git problems

--- src/userprog/mmap_file.c
+++ src/userprog/mmap_file.c
@@ -67,8 +67,8 @@ write_back_mmap_file(struct mmap_file * mmap_file)
 /* Write back a single mmaped_page. */
 void write_back_mapped_page(struct mmap_file * mmap_file, int offset, int readable_bytes)
 {
- // page_multi_pin((char*)mmap_file->vaddr + offset, readable_bytes);
+  page_multi_pin((char*)mmap_file->vaddr + offset, readable_bytes);
   file_write_at (mmap_file->file, mmap_file->vaddr, readable_bytes, offset);
- // page_multi_unpin ((char*)mmap_file->vaddr + offset, readable_bytes);
+  page_multi_unpin ((char*)mmap_file->vaddr + offset, readable_bytes);
 }
 #endif
